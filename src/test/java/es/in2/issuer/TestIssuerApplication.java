f75f1ebc621859fd3041ca4ea024350550fbef35
Oriol Canadés <83498869+oriolcanadesin2@users.noreply.github.com>
2024-10-29 19:53:38 +0100

    Initial commit

---
+++ src/test/java/es/in2/issuer/TestIssuerApplication.java
@@ -0,0 +1,11 @@
+package es.in2.issuer;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestIssuerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(IssuerApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
