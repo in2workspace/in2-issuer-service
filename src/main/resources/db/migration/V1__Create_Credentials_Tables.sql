05046336c176ad29b00b7e753804f4133d85481e
Oriol Canadés <83498869+oriolcanadesin2@users.noreply.github.com>
2024-10-30 01:00:41 +0100

    Update flyway error

--- src/main/resources/db/migration/V1__Create_Credentials_Tables.sql
+++ src/main/resources/db/migration/V1__Create_Credentials_Tables.sql
@@ -1,10 +1,11 @@
-CREATE EXTENSION IF NOT EXISTS "pgcrypto";
+SELECT version();
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 CREATE SCHEMA IF NOT EXISTS credentials;
 
 -- Create tenant table if it doesn't exist
 CREATE TABLE IF NOT EXISTS credentials.tenant
 (
-    id            UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    id UUID PRIMARY KEY DEFAULT (md5(random()::text || clock_timestamp()::text)::uuid),
     tenant_id     VARCHAR(255) NOT NULL,
     tenant_domain VARCHAR(255) NOT NULL,
     created_at    TIMESTAMP        DEFAULT CURRENT_TIMESTAMP,
